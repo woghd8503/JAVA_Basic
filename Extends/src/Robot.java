8f587ad4ba8ece7543ef6d2ab2adc7dc8d05800e
James <woghd8503@naver.com>
2022-06-02 00:42:40 +0900

    Extends

--- Extends/src/Robot.java
+++ Extends/src/Robot.java
@@ -1,20 +1,36 @@
 
 public class Robot {
- int x, y;
- void mySituation()
- {
-	 System.out.print("나의 위치 x: " + x + " , y: " + y);
- }
+	int x, y;
+
+	void mySituation() {
+		System.out.print("나의 위치 x: " + x + " , y: " + y);
+	}
 }
 
-class FishRobot extends Robot{
+class FishRobot extends Robot {
 	int depth;
-	FishRobot(int x, int y, int depth)
-	{
+
+	FishRobot(int x, int y, int depth) {
 		this.x = x;
 		this.y = y;
 		this.depth = depth;
 	}
-	
-	
+
+	void swim() {
+		System.out.print("물고기로봇 수심: " + depth + "m");
+	}
+}
+
+class DroneRobot extends Robot {
+	int altitude; // 고도
+
+	DroneRobot(int x, int y, int altitude) {
+		this.x = x;
+		this.y = y;
+		this.altitude = altitude;
+	}
+
+	void fly() {
+		System.out.print("드론로봇 높이 " + altitude + "m");
+	}
 }

24d6140befd1dee845a0109b304d09326651fe3e
James <woghd8503@naver.com>
2022-06-06 20:56:06 +0900

    Java_Basic

---
+++ AccessModifier/src/MainClass.java
@@ -0,0 +1,9 @@
+
+public class MainClass {
+	public static void main(String[] ar)
+	{
+		Childeren child1 = new Childeren();
+		//System.out.println(child1.building); // error
+		System.out.println(child1.money);
+	}
+}
---
+++ AccessModifier/src/MyParents.java
@@ -0,0 +1,9 @@
+
+public class MyParents {  // 부모 클래스 
+	private int building;
+	int money = 100000000;	
+}
+
+class Childeren extends MyParents{ // 자식 클래스
+	
+}

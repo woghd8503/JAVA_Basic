22cb5ea3e83f845158902c2911d8fa8a52246ef2
James <woghd8503@naver.com>
2022-06-06 21:07:49 +0900

    Java_Basic

--- AccessModifier/src/MainClass.java
+++ AccessModifier/src/MainClass.java
@@ -5,8 +5,11 @@ public class MainClass {
 		Childeren child1 = new Childeren();
 		Robot r1 = new Robot();
 		//System.out.println(child1.building); // error
-		System.out.println(child1.money);
+		System.out.println(r1.getter());
 		System.out.println(r1.x);
 		System.out.println(r1.y);
+		
+		r1.setter("짱가");
+		System.out.println(r1.getter());
 	}
 }
